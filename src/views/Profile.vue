202ddae55a6470cb4dbb6c8417b056fd7f515acc
Gerard Sans <gerard.sans@gmail.com>
2021-10-09 18:08:32 +0100

    Handle non-existent profiles navigation

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,14 +3,14 @@
     <div class="flex container h-screen w-full">
       <SideNav />
 
-      <div class="w-full md:w-1/2 h-full overflow-y-scroll" v-scroll:bottom="loadMore">
+      <div class="w-full md:w-2/3 lg:w-1/2 h-full overflow-y-scroll" v-scroll:bottom="loadMore">
         <div class="px-5 py-3 border-b border-lighter flex items-center">
           <button @click="gotoHome()" class="rounded-full md:pr-2 focus:outline-none hover:bg-lightblue">
             <i class="fas fa-arrow-left text-blue"></i>
           </button>
           <div class="lg:block ml-4">
-            <h1 class="text-xl font-bold">{{ profile.name }}</h1>
-            <p class="text-left text-sm leading-tight text-dark">{{ profile.tweetsCount }} Tweets</p>
+            <h1 class="text-xl font-bold">{{ profile.name || 'Profile' }}</h1>
+            <p v-if="!noUser" class="text-left text-sm leading-tight text-dark">{{ profile.tweetsCount }} Tweets</p>
           </div>
         </div>
 
@@ -26,13 +26,13 @@
         </div>
 
         <!-- profile details -->
-        <div class="p-3 flex flex-col">
-          <div class="flex flex-row justify-between">
-            <img :src="profile.imageUrl" 
+        <div class="py-3 flex flex-col">
+          <div class="px-3 flex flex-row justify-between">
+            <img v-if="profile" :src="profile.imageUrl" 
                 class="w-24 h-24 md:w-32 md:h-32 rounded-full border-white"
                 style="margin-top: -80px; border-width: 6px;"/>
 
-            <div v-if="isSelf">
+            <div v-if="isSelf && profile">
               <button v-if="profile.imageUrl === null || profile.imageUrl === 'default_profile.png'"
                       @click="setUpProfile()" 
                       class="text-xs md:text-base md:ml-auto text-blue font-bold px-4 py-2 rounded-full border border-blue mb-2 hover:bg-lightblue">
@@ -45,7 +45,7 @@
               </button>
             </div>
 
-            <div v-if="!isSelf">
+            <div v-if="!noUser && !isSelf && !loading">
               <button class="text-xs md:text-base md:ml-auto mr-1 md:mr-3 text-blue font-bold px-3 py-1 md:px-3 md:py-2 rounded-full border border-blue mb-2 hover:bg-lightblue">
                 <i class="fas fa-ellipsis-h"></i>
               </button>
@@ -66,7 +66,7 @@
               </button>
             </div>
           </div>
-          <div>
+          <div v-if="!noUser && !loading" class="px-3">
             <p class="font-bold text-xl">{{profile.name}}</p>
             <p class="text-dark">@{{profile.screenName}}<span v-if="profile.followedBy" class="text-sm font-medium bg-gray-100 py-1 px-1 mx-2 rounded text-gray-500 align-middle">Follows you</span></p>
             <p class="my-2">{{profile.bio}}</p>
@@ -84,7 +84,7 @@
                 <p class="text-dark">Joined {{joinedDate}}</p>
               </div>
             </div>
-            <div class="flex flex-row mt-1">
+            <div v-if="!noUser && !loading" class="flex flex-row mt-1">
               <button @click="goToFollowing()" class="mr-4 flex flex-row hover:underline">
                 <span class="font-bold">{{profile.followingCount}}</span>
                 <span class="text-dark whitespace-pre"> Following</span>
@@ -95,17 +95,20 @@
               </button>
             </div>
           </div>
-          <div class="flex flex-row justify-evenly mt-2">
-            <button class="text-dark font-bold border-b-2 border-blue p-1 md:px-11 md:py-4 hover:bg-lightblue">Tweets</button>
-            <button class="text-dark font-bold border-b-2 p-1 md:px-11 md:py-4 hover:bg-lightblue">Tweets & replies</button>
-            <button class="text-dark font-bold border-b-2 p-1 md:px-11 md:py-4 hover:bg-lightblue">Media</button>
-            <button class="text-dark font-bold border-b-2 p-1 md:px-11 md:py-4 hover:bg-lightblue">Like</button>
+          <div v-if="noUser" class="px-5">
+            <p class="text-black text-lg">@{{this.$route.params.screenName}}</p>
+          </div>
+          <div v-if="!noUser && !loading" class="flex flex-row justify-evenly mt-2">
+            <button class="flex-grow text-dark font-bold border-b-2 border-blue p-1 md:px-2 md:py-4 hover:bg-lightblue">Tweets</button>
+            <button class="flex-grow text-dark font-bold border-b-2 p-1 md:px-2 md:py-4 hover:bg-lightblue">Tweets & replies</button>
+            <button class="flex-grow text-dark font-bold border-b-2 p-1 md:px-2 md:py-4 hover:bg-lightblue">Media</button>
+            <button class="flex-grow text-dark font-bold border-b-2 p-1 md:px-2 md:py-4 hover:bg-lightblue">Like</button>
           </div>
         </div>
 
         <!-- tweets -->
         <Loader :loading="loading" />
-        <div v-if="!loading && tweets.length === 0" class="flex flex-col items-center justify-center w-full pt-10">
+        <div v-if="!noUser && !loading && tweets.length === 0" class="flex flex-col items-center justify-center w-full pt-10">
           <p class="font-bold text-lg"><span>{{ isSelf ? 'You': `@${profile.screenName}` }}</span> haven’t Tweeted yet</p>
           <p class="text-sm text-dark">When <span>{{ isSelf ? 'you post': `@${profile.screenName} posts` }}</span> a Tweet, it’ll show up here.</p>
           <button v-if="isSelf" class="text-white bg-blue rounded-full font-semibold mt-4 px-4 py-2 hover:bg-darkblue">
@@ -113,8 +116,14 @@
             <i class="fas fa-plus lg:hidden"></i>
           </button>
         </div>
+        <div v-if="noUser" class="flex flex-col items-center justify-center">
+          <div class="w-3/5 flex flex-col items-left mt-10">
+            <p class="font-bold text-2xl">This account doesn’t exist</p>
+            <p class="text-lg text-dark">Try searching for another.</p>
+          </div>
+        </div>
 
-        <Tweets :tweets="tweets" />
+        <Tweets v-if="!noUser" :tweets="tweets" />
       </div>
 
       <div class="hidden md:block w-1/3 z-0 h-full border-l border-lighter px-6 py-2 overflow-y-scroll relative">
@@ -157,6 +166,7 @@ export default {
       isSelf: false,
       followingLabel: 'Following',
       loading: true,
+      noUser: false,
     }
   },
   computed: {
@@ -244,10 +254,17 @@ export default {
     await this.loginUserIfAlreadyAuthenticated();
     const screenName = this.$route.params.screenName;
     this.isSelf = this.isMySelf(screenName);
+    //this.loading = false;
     await Promise.all([
       this.loadProfile(screenName),
-      this.loadTweets(screenName).then(() => this.loading = false)
+      this.loadTweets(screenName)
+        .then(()=>this.loading = false)
+        .catch(()=>{ 
+          this.loading = false;
+          this.noUser = true;
+        })
     ]);
+    
   },
 }
 </script>
