ff752f94d74813abf327de673db0ada5fa5ee369
Gerard Sans <gerard.sans@gmail.com>
2021-10-10 15:08:08 +0100

    Fix time filter

--- src/filters/time.js
+++ src/filters/time.js
@@ -7,12 +7,16 @@ moment.updateLocale('en', {
   ],
 });
 
-export default function time(date) {
-  if (moment().diff(date, "days") < 1) { // 6:30 PM
-    return moment(date).format("h:mm A");
-  } else if (moment().diff(date, "days") < 2) { // Yesterday, 6:30 PM
-    return `Yesterday, ${moment(date).format("h:mm A")}`; 
-  } else {
-    return moment(date).format("MMM D, h:mm A"); // Jan 31, 6:30 PM
+export default function time(_date) {
+  const date = moment(_date);
+  const today = moment().startOf('day');
+  const yesterday = moment().subtract(1,'day').startOf('day');
+
+  if (date <= yesterday) { // Jan 31, 6:30 PM
+    return date.format("MMM D, h:mm A");
+  } else if (date <= today) { // Yesterday, 6:30 PM
+    return `Yesterday, ${date.format("h:mm A")}`;
+  } else { // 6:30 PM
+    return date.format("h:mm A");
   }
 }
