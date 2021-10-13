cb09774e0a5faa628eb737d110134499e18da3fa
Gerard Sans <gerard.sans@gmail.com>
2021-10-13 13:43:13 +0100

    Fix to disable users not following you

--- src/components/UserNewMessage.vue
+++ src/components/UserNewMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="profile.id !== user.id" @click="user.following && selected(user)" class="w-full px-4 py-2 border-b hover:bg-lightest flex flex-row cursor-pointer" :class="!user.following?'cursor-not-allowed opacity-60':''">
+  <div v-if="profile.id !== user.id" @click="user.followedBy && selected(user)" class="w-full px-4 py-2 border-b hover:bg-lightest flex flex-row cursor-pointer" :class="!user.followedBy?'cursor-not-allowed opacity-60':''">
     <div class="flex-none">
       <img :src="`${user.imageUrl || 'default_profile.png'}`" class="h-12 w-12 rounded-full"/>
     </div>
@@ -7,7 +7,7 @@
       <div class="flex flex-row justify-between w-full">
         <div class="flex flex-col">
           <p class="font-bold">{{ user.name }}</p>
-          <p class="text-dark text-sm">@{{ user.screenName }} {{!user.following?"cant'be messaged":''}}</p>
+          <p class="text-dark text-sm">@{{ user.screenName }} {{!user.followedBy?"cant'be messaged":''}}</p>
         </div>
       </div>
     </div>
